eb750370c3938a5b7ea08d892e078bf383cd45d2
Ruicheng Lai <chucknotchunk@gmail.com>
2023-12-12 14:10:32 +0000

    code refinement

--- Motion Control/Motor_Speed_Control_WIP/Motor_Speed_Control_WIP.ino
+++ Motion Control/Motor_Speed_Control_WIP/Motor_Speed_Control_WIP.ino
@@ -8,74 +8,54 @@
 #include "initialization.h"
 
 
-#define NMOTORS 1 // define numver of motors 
+const int NMOTORS = 1; // define numver of motors 
 
 const int ENCA[] = {2};
 const int ENCB[] = {3};
+const int RPWM[] = {0};
+//const int LPWM[] = {};
+const int IN1[] = {1}; // motor driver direction pin
+const int IN2[] = {2}; // motor driver direction pin
 
 volatile int pos_i[NMOTORS];
 volatile int posPrev[NMOTORS];
 volatile float velocity[NMOTORS];
 volatile float rpm[NMOTORS];
 volatile float rpmFilt[NMOTORS];
+volatile float motorVelocity[NMOTORS];
+volatile float deltaT;
 
-long prevT = 0;
 
 int SerialInput = 0;  // Variable to store the last PWM value
 
 LowPassFilter filter1; // Filter for the first signal
-PIDController pid1(12.5, 7.5, 2); // PID controller for process 1
+PIDController pid1(12.5, 5, 2); // PID controller for process 1
 
 void setup() {
 
-  for(int k = 0; k < NMOTORS; k++){
-  pinMode(ENCA[k],INPUT);
-  pinMode(ENCB[k],INPUT);
-  }
-  
   initialization();
+
+  setupEncoders();
 
   attachInterrupt(digitalPinToInterrupt(ENCA[0]), readEncoder<0>, RISING);
 } 
 
 void loop() {
-
+  float deltaT = calculateDeltaTime(); // get deltaT from the function
 
   pwm_map(); // map the pwm input
 
-  float deltaT;
-
-  int pos[NMOTORS];
-  ATOMIC_BLOCK(ATOMIC_RESTORESTATE){
-    for(int k = 0; k < NMOTORS; k++){
-      pos[k] = pos_i[k];
-      }
-  }
-
-  // Compute velocity
-  for (int k = 0; k < NMOTORS; k++){
-    long currT = micros();
-    deltaT = ((float) (currT - prevT)) / 1.0e6;
-    velocity[k] = (pos[k] - posPrev[k]) / deltaT;
-    posPrev[k] = pos[k];
-    prevT = currT;
-
-    // Convert count/s to RPM
-    rpm[k] = velocity[k] / 600.0 * 60.0;
-
-    rpmFilt[k] = filter1.update(rpm[k]);
-    }
+  calculatevelocity(deltaT);
 
   // Set a target
   SerialInput = serial_input();
   float TargetV = SerialInput;
 
   // Compute the control signal u  
-  float u = pid1.calculate(SerialInput, rpmFilt[0], deltaT);
+  motorVelocity[0] = pid1.calculate(SerialInput, rpmFilt[0], deltaT);
   
-
   // Set the motor speed and direction
-  setMotor((u < 0) ? -1 : 1, min((int)fabs(u * 20), 4095));
+  setMotor((motorVelocity[0] < 0) ? -1 : 1, min((int)fabs(motorVelocity[0] * 20), 4095));
 
   Serial.print(TargetV);
   Serial.print(" ");
@@ -106,4 +86,33 @@ void readEncoder() {
   // Read encoder B when ENCA rises
   int b = digitalRead(ENCB[j]);
   pos_i[j] += (b > 0) ? 1 : -1;
+}
+
+float calculateDeltaTime(){
+  static unsigned long prevT = 0; // static variable to hold time of the last call
+  unsigned long currT = micros(); // get the current time
+  deltaT = ((float) (currT - prevT)) / 1.0e6; // calculate the time difference in seconds
+  prevT = currT; // update prevT for the next call
+  return deltaT; // return the calculated deltaT
+}
+
+void calculatevelocity(float deltaT){
+  int pos[NMOTORS];
+  ATOMIC_BLOCK(ATOMIC_RESTORESTATE){
+    for(int k = 0; k < NMOTORS; k++){
+      pos[k] = pos_i[k];
+      }
+  }
+
+  // Compute velocity
+  for (int k = 0; k < NMOTORS; k++){
+    long currT = micros();
+    velocity[k] = (pos[k] - posPrev[k]) / deltaT;
+    posPrev[k] = pos[k];
+
+    // Convert count/s to RPM
+    rpm[k] = velocity[k] / 600.0 * 60.0;
+
+    rpmFilt[k] = filter1.update(rpm[k]);
+    }
 }
--- Motion Control/Motor_Speed_Control_WIP/initialization.h
+++ Motion Control/Motor_Speed_Control_WIP/initialization.h
@@ -1,6 +1,10 @@
 #ifndef _INIT_H
 #define _INIT_H
 
+extern const int NMOTORS;
+extern const int ENCA[];
+extern const int ENCB[];
+
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 
 void initialization(void){
@@ -10,6 +14,14 @@ void initialization(void){
   pwm.begin();                  // Enable communication with PCA9685
   pwm.setOscillatorFrequency(27000000);  // Set onboard oscillator to 27MHz
   pwm.setPWMFreq(1600);         // Set PWM frequency to 1.6kHz (max value)
+}
+
+void setupEncoders(){
+  for(int k = 0; k < NMOTORS; k++){
+    pinMode(ENCA[k],INPUT);
+    pinMode(ENCB[k],INPUT);
+  }
+  
 }
 
 #endif 
